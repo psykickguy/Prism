e325ae985b7c51963d343a3d1d0a5bb1a2bc0292
PsyKickGuy <shubhdas2005@gmail.com>
2025-10-25 02:17:36 +0530

    temporary ui of vault

--- Frontend/src/pages/Vault.jsx
+++ Frontend/src/pages/Vault.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { AuroraText } from "@/components/ui/aurora-text";
+import { VaultList } from "@/components/VaultList";
+import Masonry from "@/components/Masonry";
+import { FocusCards } from "@/components/ui/focus-cards";
 
 export default function Import() {
   return (
     <>
-      <AuroraText>Vault</AuroraText>
+      {/* <AuroraText className="text-2xl font-bold">Vault</AuroraText> */}
+      <VaultList />
+      {/* <Masonry
+        items={items}
+        ease="power3.out"
+        duration={0.6}
+        stagger={0.05}
+        animateFrom="bottom"
+        scaleOnHover={true}
+        hoverScale={0.95}
+        blurToFocus={true}
+        colorShiftOnHover={false}
+      /> */}
     </>
   );
 }
